689d3b2cd1261f528a942563fd0939dc4266e9a9
tangshitao <zy15779@nottingham.edu.cn>
2017-05-07 20:25:03 +0800

    initial commit

---
+++ config.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<vector>
+/*
+store some common configuration values
+*/
+const double MAX_SCORE = 1e18;
+
+//the execution time for genetic algorithm
+const std::vector<int>EXECUTION_TIME = { 595,1195 };
+
+//the max number of gene in genetic algorithm
+const int MAX_GENE_NUMBER = 500;
